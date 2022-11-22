7f332d9e7b2d3f503d2cd0d77d74b0904468a486
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-22 14:28:41 +0900

    Update 0344-reverse-string.py

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,6 +1,13 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        s[:] = s[::-1]
+    def reverseString_two_pointer(self, s: List[str]) -> None:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left] = s[right]
+            s[right] = s[left]
+            left += 1
+            right -= 1
+
+    def reverseString_pytonic_way(self, s: List[str]) -> None:
+        s.reverse()
+        # s[:] = s[::-1]
