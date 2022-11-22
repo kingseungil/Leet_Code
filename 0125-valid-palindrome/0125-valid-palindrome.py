ec9170f2e2390531bb0e6669fcb16c8b38b1180e
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-22 14:09:38 +0900

    Update 0125-valid-palindrome.py

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,15 @@
+import collections
+import re
+from typing import Deque
+
+
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
+    # 1. list 활용
+    def isPalindrome_list(self, s: str) -> bool:
         strs = []
         for char in s:
             if char.isalnum():  # isalnum() : 영문자, 숫자 여부를 판별하는 함수
-                strs.append(char.lower())
+                strs.append(char.lower())  # lower() : 소문자로 바꿔주는 함수
 
         # 펠른드롬 여부 판별
         while len(strs) > 1:
@@ -11,3 +17,27 @@ class Solution:
                 return False
 
         return True
+
+    # 2.deque 활용
+    def isPalindrome_deque(self, s: str) -> bool:
+
+        # 자료형 데크로 선언
+        strs: Deque = collections.deque()
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+
+        return True
+
+    # 3.slicing 활용
+    def isPalindrome_slicing(self, s: str) -> bool:
+        s = s.lower()
+        # 정규식으로 불필요한 문자 필터링
+        s = re.sub('[^a-z0-9]', '', s)
+
+        return s == s[::-1]  # 슬라이싱
