3e5ae8c55f98a567dc8fee95bf41b8d5f2d420c7
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-25 15:48:01 +0900

    Update 0121-best-time-to-buy-and-sell-stock.py

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,20 @@
+import sys
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = sys.maxsize
+
+        # 최솟값과 최댓값을 계속 갱신
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+
+        return max_profit
+
+    # 시간 더 걸림
+    def maxProfit2(self, prices: List[int]):
         buy, sell = 0, 1
         maxP = 0
 
@@ -10,5 +25,4 @@ class Solution:
             else:
                 buy = sell
             sell += 1
-            
         return maxP
