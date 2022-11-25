c55e7752d95e1601d9fe2619dad6a6534fc41089
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-25 14:31:36 +0900

    Update 0561-array-partition.py

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 오름차순
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
         sum = 0
@@ -12,3 +13,19 @@ class Solution:
                 pair = []
 
         return sum
+
+    # 짝수 번째 값만 계산(페어에 대해 min()값을 구하지 않아도 됨)
+    def arrayPairSum2(self, nums: List[int]):
+        sum = 0
+        nums.sort()
+
+        for i, n in enumerate(nums):
+            # 짝수 번 째 값의 합 계산
+            if i % 2 == 0:
+                sum += n
+
+        return sum
+
+    # Pythonic Way -> slicing 활용
+    def arrayPairSum3(self, nums: List[int]):
+        return sum(sorted(nums)[::2])
