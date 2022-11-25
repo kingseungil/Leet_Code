3841dd6b90e3d27f5c51aa9f42061e5f828911d7
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-25 15:46:44 +0900

    Time: 3039 ms (13.73%), Space: 25 MB (39.42%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-        min_price = sys.maxsize
+        buy, sell = 0, 1
+        maxP = 0
 
-        # 최솟값과 최댓값을 계속 갱신
-        for price in prices:
-            min_price = min(min_price, price)
-            max_profit = max(max_profit, price - min_price)
-
-        return max_profit
+        while sell < len(prices):
+            if prices[buy] < prices[sell]:
+                profit = prices[sell] - prices[buy]
+                maxP = max(maxP, profit)
+            else:
+                buy = sell
+            sell += 1
+            
+        return maxP
