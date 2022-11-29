b9dfae67a4e3688b68eeb2446aa94a3e3e31997f
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-29 10:01:52 +0900

    Update 0206-reverse-linked-list.py

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # # 1. 재귀구조
+        # # 1. 재귀 구조
         # def reverse(node: ListNode, prev: ListNode = None):
         #     if not node:
         #         return prev
@@ -15,7 +15,7 @@ class Solution:
         #     return reverse(next, node)
         # return reverse(head)
     
-    # 2. 반복구조
+        # 2. 반복 구조
         node, prev = head, None
 
         while node:
