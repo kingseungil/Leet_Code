9b0e8f1572150b5f864ed2509e236d3770cb3fb7
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-29 11:21:56 +0900

    Update 0024-swap-nodes-in-pairs.py

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -4,7 +4,38 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # 풀이 1 : 값만 교환하는 방식(권장 X)
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+
+        while cur and cur.next:
+            # 값만 교환
+            cur.val, cur.next.val = cur.next.val, cur.val
+            cur = cur.next.next
+
+        return head
+
+    # 풀이 2: 반복 구조
+    def swapPairs(self, head: Optional[ListNode]):
+        root = prev = ListNode(None)
+        prev.next = head
+        while head and head.next:
+            # b가 a(head)를 가리키도록 할당
+            b = head.next
+            head.next = b.next
+            b.next = head
+
+            # prev가 b를 가리키도록 할당
+            prev.next = b
+
+            # 다음번 비교를 위해 이동
+            head = head.next
+            prev = prev.next.next
+
+        return root.next
+
+    # 풀이 3: 재귀 구조
+    def swapPairs(self, head: Optional[ListNode]):
         if head and head.next:
             p = head.next
             # 스왑된 값 리턴 받음
