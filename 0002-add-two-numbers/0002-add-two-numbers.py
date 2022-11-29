651f69287f64cff1202bcd3064b63ff49c4038d5
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-29 10:41:47 +0900

    Update 0002-add-two-numbers.py

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -43,3 +43,27 @@ class Solution:
 
         # 최종 계산 결과 연결 리스트 변환
         return self.toReversedLinkedList(str(resultStr))
+    
+    ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+    
+    # 전가산기 풀이
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode):
+        root = head = ListNode(0)
+
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            # 두 입력값의 합 계산
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            # 몫(자리 올림수)과 나머지(값) 계산
+            carry, val = divmod(sum+carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+
+        return root.next
