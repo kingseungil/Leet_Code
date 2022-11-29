b3d401a3184674dead1b39564b321c9ee4eeca87
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-29 09:57:55 +0900

    Time: 73 ms (40.98%), Space: 15.3 MB (94.35%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,13 +5,22 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # 1. 재귀구조
-        def reverse(node: ListNode, prev: ListNode = None):
-            if not node:
-                return prev
-            # node.next에는 이전 prev 리스트를 계속 연결
+        # # 1. 재귀구조
+        # def reverse(node: ListNode, prev: ListNode = None):
+        #     if not node:
+        #         return prev
+        #     # node.next에는 이전 prev 리스트를 계속 연결
+        #     next, node.next = node.next, prev
+        #     # 다음 노드 next와 현재 노드 node를 파라미터로 지정한 함수를 계속해서 재귀 호출
+        #     return reverse(next, node)
+        # return reverse(head)
+    
+    # 2. 반복구조
+        node, prev = head, None
+
+        while node:
             next, node.next = node.next, prev
-            # 다음 노드 next와 현재 노드 node를 파라미터로 지정한 함수를 계속해서 재귀 호출
-            return reverse(next, node)
-        return reverse(head)
+            prev, node = node, next
+
+        return prev
         
